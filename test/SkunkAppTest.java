8fcc8cf34c04bb0c969a9f379bf72337a80fcc33
Justin <huixx058@umn.edu>
2017-10-28 12:06:18 -0500

    new changes update

--- test/SkunkAppTest.java
+++ test/SkunkAppTest.java
@@ -14,4 +14,31 @@ public class SkunkAppTest {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void new_dice_can()
+	{
+		Dice d = new Dice();
+		assertTrue("new Dice instance not null",d!=null);
+		
+	}
+	
+	@Test
+	public void new_dice_will_roll_double_skunk_first() {
+		Dice d=new Dice(new int[] {1}, new int[] {1});
+		d.roll();
+		assertEquals("first die is not 1", 1, d.getDie1Roll());
+		assertEquals("second die is not 1", 1, d.getDie2Roll());
+		
+	}
+	
+	@Test
+	public void new_dice_rolls_boxcars_then_double_skunk(){
+		Dice d = new Dice(new int[] {6,1}, new int[] {6,1});
+		d.roll();
+		assertEquals("first die is not 6", 6, d.getDie1Roll());
+		assertEquals("second die is not 6", 6, d.getDie2Roll());
+		d.roll();
+		assertEquals("first die is not 1", 1, d.getDie1Roll());
+		assertEquals("second die is not 1", 1, d.getDie2Roll());
+	}
 }
